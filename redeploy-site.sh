a03757a28e76c7613b7e477529b425358313a792
Thrinayani39e <thrinayaniyedhoti@gmail.com>
2026-07-09 05:51:40 +0000

    Update redeploy script for systemd

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Kill tmux
-tmux kill-server
-
-# Move to the project folder
+# 1. cd into your project folder
 cd /root/practice_portfolio
 
-# Fetch latest changes
+# 2. Pull the latest changes
 git fetch && git reset origin/main --hard
 
-# Activate using the correct folder name
-source /root/practice_portfolio/python3-virtualenv/bin/activate
+# 3. Enter the virtual environment and install dependencies
+./python3-virtualenv/bin/pip install -r requirements.txt
 
-# Install requirements
-/root/practice_portfolio/python3-virtualenv/bin/pip install -r requirements.txt
-
-# Start Flask
-tmux new-session -d -s flask-session 'source /root/practice_portfolio/python3-virtualenv/bin/activate && export FLASK_APP=app && flask run --host=0.0.0.0'
+# 4. Restart the myportfolio service
+systemctl restart myportfolio
